8ae5f7805cd2c431ece0e5f53838f8bea5714b62
Giftea <eleojogiftuhiene@gmail.com>
2022-08-25 16:32:39 +0100

    added redirect

--- frontend/App.js
+++ frontend/App.js
@@ -2,7 +2,7 @@ import "regenerator-runtime/runtime";
 import React from "react";
 import "./assets/global.css";
 import { BrowserRouter } from "react-router-dom";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Redirect } from "react-router-dom";
 import Home from "./pages/Home";
 import AddBlog from "./pages/AddBlog";
 import Blog from "./pages/Blog";
@@ -11,9 +11,10 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/add" element={<AddBlog />} />
-      <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<AddBlog />} />
+        <Route path="/blog/:id" element={<Blog />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
