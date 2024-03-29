6928efe8e35ff0f7cf864623d3a325dfbd649e4c
giftea <eleojogiftuhiene@gmail.com>
2024-03-29 20:13:30 +0100

    added App.js

---
+++ frontend/App.js
@@ -0,0 +1,13 @@
+import "regenerator-runtime/runtime";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+          {/* Routes will go here */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
