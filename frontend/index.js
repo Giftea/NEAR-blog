985d5fee3eeeaa88fa4cc4e29e1bce22d3d43943
giftea <eleojogiftuhiene@gmail.com>
2024-03-29 17:29:20 +0100

    Frontend starter init

--- frontend/index.js
+++ frontend/index.js
@@ -1,15 +1,4 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import { initContract } from './near-api';
+import React from "react"
+import ReactDOM from "react-dom"
 
-const reactRoot = createRoot(document.querySelector('#root'));
-
-window.nearInitPromise = initContract()
-  .then(() => {
-    reactRoot.render(<App />);
-  })
-  .catch(e => {
-    reactRoot.render(<div style={{color: 'red'}}>Error: <code>{e.message}</code></div>);
-    console.error(e);
-  });
+ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"))
